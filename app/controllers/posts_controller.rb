a28be7ec72629e3958184dca0639ff82c8616e6f
Ece Ozalp <github email address>
2016-09-15 16:12:35 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,19 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}])}
+    end
   end
 
   def new
     @post = Post.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @post }
+    end
   end
 
   def create
@@ -26,9 +35,14 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  # def post_data
+  #   post = Post.find(params[:id])
+  #   render json: PostSerializer.serialize(post)
+  # end
+
   def post_data
     post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+    render json: post.to_json(only: [:title, :description, :id], include: [ author: { only: [:name]}])
   end
 
 private
@@ -39,6 +53,6 @@ private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
